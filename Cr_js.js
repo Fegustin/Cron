812765cceb93538ceae27b73580bbc21f98a5c4d
Fegustin <ziotyr1@gmail.com>
2019-07-08 14:30:19 +0300

    cron

---
+++ Cr_js.js
@@ -0,0 +1,43 @@
+function cl_btn() {
+
+    var cl_info_1 = document.getElementById('clients');
+    cl_info_1.innerHTML = "<p>Имя: </p> <p>Домен: </p> <p>Почта: </p> <p>Телефон: </p><hr>";
+
+    var cl_info_2 = document.getElementById('about');
+    cl_info_2.innerHTML = "<p>Магомед-Салам Капиев </p> <p><a href='mgstone.com'>mgstone.com</a> </p> <p><a href='mailTo: mskapiev@mail.ru'>mskapiev@mail.ru</a></p> <p> <a href='tel:+79031280746'>+7(903)-128-07-46</a> </p><hr>";
+
+    var cl_btn_add = document.getElementById('btn_add');
+    cl_btn_add.innerHTML = "<button type='button' class='btn  btn-sm btn-outline-success'>Добавить</button>";
+}
+
+function ad_btn() {
+    var ad_info_1 = document.getElementById('clients');
+    ad_info_1.innerHTML = "";
+
+    var ad_info_2 = document.getElementById('about');
+    ad_info_2.innerHTML = "";
+}
+
+function rq_btn() {
+    var ad_info_1 = document.getElementById('clients');
+    ad_info_1.innerHTML = "";
+
+    var ad_info_2 = document.getElementById('about');
+    ad_info_2.innerHTML = "";
+}
+
+function st_btn() {
+    var ad_info_1 = document.getElementById('clients');
+    ad_info_1.innerHTML = "";
+
+    var ad_info_2 = document.getElementById('about');
+    ad_info_2.innerHTML = "";
+}
+
+function smm_btn() {
+    var ad_info_1 = document.getElementById('clients');
+    ad_info_1.innerHTML = "";
+
+    var ad_info_2 = document.getElementById('about');
+    ad_info_2.innerHTML = "";
+}
