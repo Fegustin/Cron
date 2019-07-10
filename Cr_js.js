5fab4a5af30b862bb752fde4acba18681b0eda2a
Fegustin <ziotyr1@gmail.com>
2019-07-10 13:39:08 +0300

    Cron 1.0.1

--- Cr_js.js
+++ Cr_js.js
@@ -41,3 +41,75 @@ function smm_btn() {
     var ad_info_2 = document.getElementById('about');
     ad_info_2.innerHTML = "";
 }
+
+
+
+
+
+
+
+// Форма регистрации
+const registration = document.querySelector('.registration');
+
+//   data-toggle="modal"
+registration.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let regEmail = document.querySelector('.registration__email');
+    let regPassword = document.querySelector('.registration__password');
+    let regPasswordRepeat = document.querySelector('.registration__password-repeat');
+
+    if (regPassword.value == regPasswordRepeat.value) {
+        let departureForm = {
+            email: regEmail.value,
+            password: regPassword.value,
+        };
+
+        let dataForm = new FormData();
+        dataForm.append("json", JSON.stringify(departureForm));
+
+        fetch('', {
+            method: 'POST',
+            body: dataForm
+        }).then(res => {
+            if (res.ok) return res.json();
+            else return Promise.reject(new Error(`Respinse failed: ${res.status} /// (${res.statusText})`));
+        }).then(data => {})
+    }
+});
+
+// Форма входа
+let switcher = document.querySelector('.box__reg');
+let mainContent = document.querySelector('.main');
+const signIn = document.querySelector('#reg');
+signIn.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // let email = document.querySelector('.sing__email')
+    // let password = document.querySelector('.sing__password')
+
+    switcher.className += ' dontSing';
+    mainContent.className += ' activeMain';
+
+
+    // let departureForm = {
+    //     email: email.value,
+    //     password: password.value,
+    // };
+
+    // let dataForm = new FormData();
+    // dataForm.append("json", JSON.stringify(departureForm));
+
+    // fetch('', {
+    //     method: 'POST',
+    //     body: dataForm
+    // }).then(res => {
+    //     if (res.ok) return res.json();
+    //     else return Promise.reject(new Error(`Respinse failed: ${res.status} /// (${res.statusText})`));
+    // }).then(data => {
+    //     if (data == 1) {
+
+    //     }
+    // })
+    
+});
